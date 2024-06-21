9db8fd9e1213067abd7850cbb1ad78fbaaeea2af
sergiojerez03 <sergio03jerez@gmail.com>
2024-06-21 11:54:36 -0500

    Pagina-Web_Version-1

---
+++ js/custom.js
@@ -0,0 +1,16 @@
+function getYear() {
+    var currentDate = new Date();
+    var currentYear = currentDate.getFullYear();
+    document.querySelector("#displayYear").innerHTML = currentYear;
+}
+
+getYear();
+
+
+function myMap() {
+    var mapProp = {
+        center: new google.maps.LatLng(40.712775, -74.005973),
+        zoom: 18,
+    };
+    var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
+}
